08b3f54049ec54908c3d3620e36cac52b6f03800
Viswa-Prakash <prakashviswa1990@gmail.com>
2023-12-07 21:25:05 +0530

    cookie-cutter initiation completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using cookie-cutter and data version control',
+    author='Viswa-Prakash',
+    license='',
+)
